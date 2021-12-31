6e63352d19e0cbf041a40f891eb3f5b7517025e3
Amita <panwar.amita@gmail.com>
2021-12-30 23:15:09 -0500

    updated the database url

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,7 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('messenger', 'postgres', 'amita', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 9,
-    min: 0,
-    idle: 10000
-  }
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-//   logging: false
-// });
-db.authenticate().then(() => {
-  console.log("Success!");
-}).catch((err) => {
-  console.log(err);
-});
+
 module.exports = db;
